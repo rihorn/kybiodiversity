3bab7886b34d429f8272c31fe3933ee7aaf16b93
Horn <Ian.Horn@ky.gov>
2018-03-27 15:33:53 -0400

    window.scroll(0, document.documentElement.scrollHeight)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 (function () {
+
+  window.scroll(0, document.documentElement.scrollHeight)
+  
   L.mapbox.accessToken =
     'pk.eyJ1IjoiaWFuaG9ybiIsImEiOiJjamRocmdseWUxMjJ0MnlwNDdiMDd5ZXZzIn0.jBWD0r86k4pBSymNrCKfzw';
   var map = L.mapbox.map('map', 'mapbox.pirates', {
